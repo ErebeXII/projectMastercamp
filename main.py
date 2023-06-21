0ee5808af52ef0e5667446e57bae0aa5099c2621
Timothé <timothe.perier2403@gmail.com>
2023-06-21 11:45:37 +0200

    create_global_csv

--- main.py
+++ main.py
@@ -13,13 +13,20 @@ def create_global_csv(path):
                            '4 Articles CGI', '5 Articles CGI', 'Code voie', 'Voie', 'Commune',
                            'Identifiant local','Type local']
         chunk = pd.read_csv(filename, sep='|', header=0, low_memory=False)
+        # drop unwanted columns
         chunk.drop(columns_to_drop, axis=1, inplace=True)
+        # drop rows with all NaN values
+        chunk.dropna(how='all', inplace=True)
+        # drop duplicates
+        chunk.drop_duplicates(inplace=True)
 
         chunks.append(chunk)
 
     # Concatenate the chunks into a single DataFrame
     df = pd.concat(chunks)
-    df.to_csv(path,
-              sep='|', encoding='utf-8', header=True, index=False)
+    df.to_csv(path, sep='|', encoding='utf-8', header=True, index=False)
 
 create_global_csv(r'C:\Users\timot\OneDrive\Documents\L3\machine_learning\projectMastercamp\VFglobal.csv')
+
+df = pd.read_csv("VFglobal.csv", sep='|', header=0, low_memory=False)
+print(df.shape)
