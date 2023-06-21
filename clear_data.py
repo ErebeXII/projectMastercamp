96244bdfc88abadc217e865a5f8c1dbad3adfa34
meder <mederic.peniguel@gmail.com>
2023-06-21 15:54:54 +0200

    clear data and correlation matrix

--- clear_data.py
+++ clear_data.py
@@ -1,10 +1,8 @@
 import numpy as np
-from main import df
+import pandas as pd
 
 
-def remplace_text(col):
-    df[col] = df[col].fillna(0)
-    df[col] = df[col].replace(np.nan, 0)
+def remplace_text(df,col):
     t = list(df[col].unique())
     if 0 in t:
         t[t.index(0)], t[0] = t[0], t[t.index(0)]
@@ -13,12 +11,25 @@ def remplace_text(col):
     return df
 
 
-def remplace_date():
+def remplace_date(df):
     df['Date mutation'] = pd.to_datetime(df['Date mutation'], format='%d/%m/%Y')
     df['Date mutation'] = df['Date mutation'].dt.strftime('%Y%m')
     df['Date mutation'] = df['Date mutation'].astype(int)
     return df
 
+
+def string_to_float_number(df, col):
+    df[col] = df[col].fillna(0)
+    df[col] = df[col].str.replace(',', '.').astype(float)
+    return df
+
+
+def string_to_int(df, col):
+    df[col] = df[col].fillna(0)
+    df[col] = pd.to_numeric(df[col], errors='coerce')
+    df[col] = df[col].fillna(0)
+    df[col] = df[col].astype(int)
+    return df
 """
 df = remplace_text('Type de voie')
 df = remplace_text('Nature mutation')
@@ -26,5 +37,5 @@ df = remplace_text('Nature culture')
 df = remplace_text('Nature culture speciale')
 df = remplace_text('B/T/Q')"""
 
-print(df.info())
+#print(df.info())
 #remplace_date()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from clear_data import remplace_text, remplace_date, string_to_float_number, string_to_int
 
 
 def create_global_csv(path):
@@ -7,10 +8,11 @@ def create_global_csv(path):
 
     # Iterate over the files and append chunks to the list
     for i in range(18, 23):
+        print(i)
         filename = f"valeursfoncieres-20{i}.txt"
         columns_to_drop = ['Identifiant de document', 'Reference document', '1 Articles CGI', '2 Articles CGI',
                            '3 Articles CGI',
-                           '4 Articles CGI', '5 Articles CGI', 'Code voie', 'Voie', 'Commune',
+                           '4 Articles CGI', '5 Articles CGI', 'Code voie', 'Voie', 'Commune','Code departement',
                            'Identifiant local','Type local']
         chunk = pd.read_csv(filename, sep='|', header=0, low_memory=False)
         # drop unwanted columns
@@ -23,10 +25,39 @@ def create_global_csv(path):
         chunks.append(chunk)
 
     # Concatenate the chunks into a single DataFrame
+
     df = pd.concat(chunks)
+    df = df.fillna(0)
+    df = remplace_text(df, 'Type de voie')
+    df = remplace_text(df, 'Nature mutation')
+    df = remplace_text(df, 'Nature culture')
+    df = remplace_text(df, 'Nature culture speciale')
+    df = remplace_text(df, 'B/T/Q')
+    df = remplace_text(df, 'Section')
+    df = string_to_int(df, 'No Volume')
+    df = string_to_int(df, '1er lot')
+    df = string_to_int(df, '2eme lot')
+    df = string_to_int(df, '3eme lot')
+    df = string_to_int(df, '4eme lot')
+    df = string_to_int(df, '5eme lot')
+    df = remplace_date(df)
+    df = string_to_float_number(df, 'Valeur fonciere')
+    df = string_to_float_number(df, 'Surface Carrez du 1er lot')
+    df = string_to_float_number(df, 'Surface Carrez du 2eme lot')
+    df = string_to_float_number(df, 'Surface Carrez du 3eme lot')
+    df = string_to_float_number(df, 'Surface Carrez du 4eme lot')
+    df = string_to_float_number(df, 'Surface Carrez du 5eme lot')
+
     df.to_csv(path, sep='|', encoding='utf-8', header=True, index=False)
 
-create_global_csv(r'C:\Users\timot\OneDrive\Documents\L3\machine_learning\projectMastercamp\VFglobal.csv')
+create_global_csv(r'C:\Users\meder\Desktop\chromedriver\VFglobal.csv')
 
-df = pd.read_csv("VFglobal.csv", sep='|', header=0, low_memory=False)
+df = pd.read_csv("C:\\Users\\meder\Desktop\\chromedriver\\VFglobal.csv", sep='|', header=0, low_memory=False)
+print(df.info())
+df_corr = df.corr()
+print("testttt", df_corr.unstack()[(df_corr.unstack() < 0.7)].nlargest(20))
+
+print(df_corr)
 print(df.shape)
+
+print(df_corr.loc['Valeur fonciere'])
