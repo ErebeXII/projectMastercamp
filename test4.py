67e029e964113da826e060ff13b7558fb4b55bfb
timot <timothe.perier2403@gmail.com>
2023-06-23 18:17:20 +0200

    test 4

--- test4.py
+++ test4.py
@@ -7,16 +7,34 @@ data = df[['Type de voie', 'Valeur fonciere']]
 
 value_counts = data['Type de voie'].value_counts()
 
-top_categories = value_counts.head(10).index.tolist()
+# create a new category called 'other' for all categories with less than 1000 values
+# data['Type de voie'] = data['Type de voie'].apply(lambda x: x if value_counts[x] > 1000 else '-1')
 
-data.loc[~data['Type de voie'].isin(top_categories), 'Type de voie'] = 'Autre'
-
+median = data.groupby('Type de voie')['Valeur fonciere'].median()
 mean_values = data.groupby('Type de voie')['Valeur fonciere'].mean()
 
-plt.figure(figsize=(12, 6))
-mean_values.plot(kind='bar', color='blue')
-plt.xlabel('Type de voie')
-plt.ylabel('Valeur foncière moyenne')
-plt.title('Comparaison de la valeur foncière par type de voie')
-plt.xticks(rotation=45)
+# order mean_values by descending order
+mean_values = mean_values.sort_values(ascending=False)
+median = median.sort_values(ascending=False)
+print(mean_values)
+print(median)
+
+# create a dictionary with the index as keys and the values as values for the mean and median values
+mean_dict = mean_values.to_dict()
+median_dict = median.to_dict()
+
+#replace the translation in the json file
+
+
+# replace the values in the data frame with the median values
+data['Type de voie'] = data['Type de voie'].replace(median_dict)
+
+# correlation matrix between "Type de voie" and "Valeur fonciere"
+corr_matrix = data.corr()
+print(corr_matrix)
+
+# plot the correlation matrix
+plt.plot(corr_matrix)
+
+
 plt.show()
